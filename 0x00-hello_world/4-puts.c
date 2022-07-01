64a98018f312598e0f02a03c253f9b3051d3e38e
ArchillesX <psalmmyace@gmail.com>
2022-06-30 17:58:37 -0700

    modify

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
+
+
 
 /**
  *
- * * main -entry point
+ *  * main - Entry point
  *
- * * Return: Always 0 (sucess)
+ *   * Return: Always 0 (Success)
  *
- * */
+ *    */
+
+
 
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+	 puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+	   return (0);
 
 }
