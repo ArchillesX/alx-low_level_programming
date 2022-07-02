3b4ed0b7d236aea4d79e321103482d240d5b6f8b
ArchillesX <psalmmyace@gmail.com>
2022-07-01 17:07:28 -0700

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Description: Alphabets
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter == 'e')
+		{
+			letter++;
+		}
+		else if (letter == 'q')
+		{
+			letter++;
+		}
+		else
+		{
+			putchar(letter;)
+			letter++;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+
+}
